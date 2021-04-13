082da20548b7f1b057382df1341f9552301e9f97
Irongade <adeoluayangade@yahoo.com>
2021-04-13 23:02:39 +0100

    Added canvas to the Home banner component

---
+++ src/components/homepage/HomeBanner.js
@@ -0,0 +1,120 @@
+import React, {useRef, useEffect} from 'react';
+
+import {Banner, Video, Canvas, BannerTitle, Headline} from "../../styles/homeStyles"
+
+// assets
+import VideoMP4 from "../../assets/video/video.mp4"
+
+//Context
+import { useGlobalStateContext } from "../../context/globalContext";
+
+// hooks
+import useWindowSize from "../../hooks/useWindowSize"
+
+const HomeBanner = ({onCursorHover}) => {
+
+    let canvasRef = useRef(null);
+    const size = useWindowSize();
+    const { currentTheme } = useGlobalStateContext()
+    
+
+    useEffect(() => {
+
+       
+        const video = document.getElementById("video")
+        let renderingElement = canvasRef.current;
+        let drawingElement = renderingElement.cloneNode();
+
+        let drawingCtx = drawingElement.getContext("2d");
+        let renderingCtx = renderingElement.getContext("2d");
+
+
+
+        let lastX;
+        let lastY;
+
+        let moving = false;
+        
+        renderingCtx.globalCompositeOperation = "source-over";
+        renderingCtx.fillStyle = currentTheme === "dark" ? "#000000" : "#ffffff";
+        renderingCtx.fillRect(0,0, size.width, size.height)
+
+        renderingElement.addEventListener("mouseover", e => {
+            moving = true;
+            lastX = e.pageX - renderingElement.offsetLeft;
+            lastY = e.pageY - renderingElement.offsetTop;
+        })
+
+        renderingElement.addEventListener("mouseup", e => {
+            moving = false;
+            lastX = e.pageX - renderingElement.offsetLeft;
+            lastY = e.pageY - renderingElement.offsetTop;
+        })
+
+        renderingElement.addEventListener("mousemove", e => {
+            if(moving) {
+                drawingCtx.globalCompositeOperation = "source-over";
+                renderingCtx.globalCompositeOperation = "destination-out";
+
+                let currentX = e.pageX - renderingElement.offsetLeft;
+                let currentY = e.pageY - renderingElement.offsetTop;
+
+                drawingCtx.lineJoin = "round";
+                drawingCtx.moveTo(lastX, lastY);
+                drawingCtx.lineTo(currentX, currentY);
+                drawingCtx.closePath();
+                drawingCtx.lineWidth = 120;
+                drawingCtx.stroke();
+                lastX = currentX;
+                lastY = currentY;
+                renderingCtx.drawImage(drawingElement, 0, 0)
+
+
+            }
+        })
+    }, [currentTheme])
+
+    const parent = {
+        initial: { y: 800 },
+        animate: {
+            y: 0,
+            transition: {
+                staggerChildren: 0.2
+            }
+        }
+    }
+
+    const child = {
+        initial: { y: 800 },
+        animate: {
+            y: 0,
+            transition: {
+                duration: 1,
+                ease: [.6, .05, -.01, .9]
+            }
+        }
+    }
+
+    return (
+        <Banner>
+            <Video>
+                <video 
+                    id="video"
+                    height="100%"
+                    width="100%"
+                    muted 
+                    loop
+                    autoPlay
+                    src={VideoMP4}
+                />
+            </Video>
+            <Canvas id="canvas" height={size.height} width={size.width} ref={canvasRef} onMouseEnter={() => onCursorHover("hovered")} onMouseLeave={onCursorHover} />
+            <BannerTitle variants={parent} initial="initial" animate="animate">
+                <Headline variants={child}> DIG </Headline>
+                <Headline variants={child}> DEEP </Headline>
+            </BannerTitle>
+        </Banner>
+    )
+}
+
+export default HomeBanner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import Layout from "../components/Layout";
 
+// contexts
+import {useGlobalStateContext, useGlobalDispatchContext} from "../context/globalContext"
+//components
+
+import HomeBanner from "../components/homepage/HomeBanner"
+
 export default function Home() {
-  return <Layout>Hey guys hope you are ready to learn!</Layout>
+
+  const {cursorStyles} = useGlobalStateContext();
+  const dispatch = useGlobalDispatchContext();
+
+  const onCursorHover = (targetCursorType) => {
+    const cursorType = (cursorStyles.includes(targetCursorType) && targetCursorType) || false;
+
+    dispatch({type: "CURSOR_TYPE", cursorType: cursorType})
+  }
+
+  return <Layout>
+    <HomeBanner onCursorHover={onCursorHover} />
+  </Layout>
 }
