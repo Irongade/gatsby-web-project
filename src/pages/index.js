dbf10c659eb53d8d2c9dc5521e52f56075edb16a
Irongade <adeoluayangade@yahoo.com>
2021-04-16 21:53:27 +0100

    fix useEffect rendering issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import Layout from "../components/Layout";
 
 // contexts
@@ -12,6 +12,14 @@ import HomeAbout from "../components/homepage/HomeAbout"
 
 export default function Home() {
 
+
+  const [isClient, setClient] = useState(false);
+  // const key = isClient ? "client" : "server";
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
   const {cursorStyles} = useGlobalStateContext();
   const dispatch = useGlobalDispatchContext();
 
@@ -20,6 +28,8 @@ export default function Home() {
 
     dispatch({type: "CURSOR_TYPE", cursorType: cursorType})
   }
+
+  if ( !isClient ) return null;
 
   return <Layout>
     <HomeBanner onCursorHover={onCursorHover} />
